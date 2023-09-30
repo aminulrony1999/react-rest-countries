8c6f03906444f47c3e84ee57afd3bd5db39ab37d
aminulrony1999 <aminulrony1999@gmail.com>
2023-09-30 15:36:01 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Rcountries></Rcountries>
     </div>
   );
 }
-
+function Rcountries(){
+  const [countries, setCountries] = useState([]);
+  useEffect( () => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => setCountries(data))
+  },[])
+  return(
+    <div>
+      <p>Total countries : {countries.length} </p>
+      {
+        countries.map(country => <Country name ={country.name.common}></Country>)
+      }
+    </div>
+  )
+}
+function Country(props){
+  return(
+    <div style ={{backgroundColor: 'lightpink',padding : '15px', border : '3px solid maroon', margin : '15px',borderRadius: '200px'}}>
+      {
+        <h2>Name : {props.name}</h2>
+      }
+    </div>
+  )
+}
 export default App;
